325af865d420f0a1e642f2aba17f25273771b44a
ussko <ussko@yandex.ru>
2020-10-16 14:22:01 +0300

    refactor: do not use matrix dimensions as template params

--- matrix2d.h
+++ matrix2d.h
@@ -3,36 +3,73 @@
 #include <ostream>
 #include <sstream>
 
-template <typename T, size_t ROW, size_t COL>
+template <typename T>
 class Matrix2d
 {
 public:
-    Matrix2d()
-    :   _data{new T[ROW * COL]}
+    Matrix2d(const size_t col_size, const size_t row_size)
+    :   _row_size{row_size},
+        _col_size{col_size},
+        _data{new T[_row_size * _col_size]}
     {
     }
     Matrix2d(const Matrix2d &rhs)
-    :   Matrix2d()
+    :   Matrix2d(rhs._col_size, rhs._row_size)
     {
-        for (size_t i = 0; i < ROW * COL; ++i)
+        for (size_t i = 0; i < _row_size * _col_size; ++i)
             _data[i] = rhs._data[i];
     }
     Matrix2d(Matrix2d &&rhs) noexcept
+    :   _row_size{rhs._row_size},
+        _col_size{rhs._col_size},
+        _data{rhs._data}
     {
-        _data = rhs._data;
         rhs._data = nullptr;
+        rhs._row_size = 0;
+        rhs._col_size = 0;
     }
     ~Matrix2d()
     {
         delete[] _data;
     }
 
-    T& operator[](size_t n)
+    // helper class for operator[]
+    class Row
     {
-        if (n >= ROW * COL)
-            throw std::range_error(dynamic_cast<std::ostringstream &&>(std::ostringstream{} << "index " << n << " out of range").str());
-        return _data[n];
+    public:
+        Row(const size_t size, T * const data)
+        :   _size{size},
+            _data{data}
+        {}
+        T& operator[](const size_t col)
+        {
+            if (col >= _size)
+                throw std::range_error(dynamic_cast<std::ostringstream &&>
+                                       (std::ostringstream{} << "col index " << col
+                                        << " out of range").str());
+            return _data[col];
+        }
+
+    private:
+        size_t _size{0};
+        T *_data{nullptr};
+    };
+
+    // to get an element use matrix[row][col]
+    Row operator[](size_t row) const
+    {
+        if (row >= _col_size)
+            throw std::range_error(dynamic_cast<std::ostringstream &&>
+                                   (std::ostringstream{} << "row index " << row
+                                                         << " out of range").str());
+        return Row{_row_size, _data + row * _row_size};
     }
+    T& at(size_t row, size_t col)
+    {
+        return (*this)[row][col];
+    }
+
+    // range-for
     T* begin()
     {
         return _data;
@@ -41,16 +78,22 @@ public:
     {
         if (!_data)
             return nullptr;
-        return _data + (ROW * COL);
+        return _data + (_row_size * _col_size);
     }
+
+    // assignment
     Matrix2d &operator=(const Matrix2d &rhs)
     {
         if (&rhs == this)
             return *this;
         if (_data)
             delete[] _data;
-        _data = new T[ROW * COL];
-        for (size_t i = 0; i < ROW * COL; ++i)
+
+        _row_size = rhs._row_size;
+        _col_size = rhs._col_size;
+        _data = new T[_row_size * _col_size];
+
+        for (size_t i = 0; i < _row_size * _col_size; ++i)
             _data[i] = rhs._data[i];
         return *this;
     }
@@ -60,37 +103,70 @@ public:
             return *this;
         if (_data)
             delete[] _data;
+
+        _row_size = rhs._row_size;
+        _col_size = rhs._col_size;
         _data = rhs._data;
+
+        rhs._row_size = 0;
+        rhs._col_size = 0;
         rhs._data = nullptr;
+
         return *this;
     }
-    friend Matrix2d operator+(const Matrix2d &lhs, const Matrix2d &rhs)
+
+    // addition
+    friend Matrix2d operator+(Matrix2d &lhs, Matrix2d &rhs)
     {
-        auto result = Matrix2d{};
-        for (size_t i = 0; i < ROW * COL; ++i)
-            result._data[i] = lhs._data[i] + rhs._data[i];
+        auto result = Matrix2d{std::max(lhs._col_size, rhs._col_size),
+                               std::max(lhs._row_size, rhs._row_size)};
+        for (size_t row = 0; row < lhs._col_size; ++row)
+            for (size_t col = 0; col < lhs._row_size; ++col)
+                result[row][col] += lhs[row][col];
+        for (size_t row = 0; row < rhs._col_size; ++row)
+            for (size_t col = 0; col < rhs._row_size; ++col)
+                result[row][col] += rhs[row][col];
         return result;
     }
     Matrix2d& operator+=(const Matrix2d &rhs)
     {
-        for (size_t i = 0; i < ROW * COL; ++i)
-            _data[i] += rhs._data[i];
+        auto col_size_new = std::max(_col_size, rhs._col_size);
+        auto row_size_new = std::max(_row_size, rhs._row_size);
+        auto data_new = new T[row_size_new * col_size_new];
+
+        for (size_t i = 0; i < row_size_new * col_size_new; ++i)
+            data_new[i] = T{};
+
+        for (size_t row = 0; row < _col_size; ++row)
+            for (size_t col = 0; col < _row_size; ++col)
+                data_new[row * row_size_new + col] = _data[row * _row_size + col];
+
+        for (size_t row = 0; row < rhs._col_size; ++row)
+            for (size_t col = 0; col < rhs._row_size; ++col)
+                data_new[row * row_size_new + col] += rhs._data[row * rhs._row_size + col];
+
+        delete [] _data;
+        _data = data_new;
+        _row_size = row_size_new;
+        _col_size = col_size_new;
+
         return *this;
     }
+
+    // print
     friend std::ostream& operator<<(std::ostream& os, const Matrix2d &m)
     {
-        for (size_t row = 0; row < ROW; ++row)
+        for (size_t row = 0; row < m._col_size; ++row)
         {
-            for (size_t col = 0; col < COL; ++col)
-                os << m._data[row * COL + col] << ' ';
+            for (size_t col = 0; col < m._row_size; ++col)
+                os << m[row][col] << ' ';
             os << '\n';
         }
         return os;
     }
 
 private:
+    size_t _row_size{0};
+    size_t _col_size{0};
     T *_data{nullptr};
 };
-
-template <size_t ROW, size_t COL>
-using Matrix2dInt = class Matrix2d<int, ROW, COL>;
